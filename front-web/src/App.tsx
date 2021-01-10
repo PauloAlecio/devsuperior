6ed8c186bfe3ae2adb2a1eb8d35665b2c7025ffb
Paulo Silva <pauloalecioreuel@gmail.com>
2021-01-10 09:13:58 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Orders/Routes';
 
 
 function App() {
   return (
-    < >
-      
-     <Navbar/>
-      <Home/>
-    </>
+
+      <Routes/>
+
   );
 }
 
